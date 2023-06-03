b2ff84304a638647a97fc9c557b0a9ded3df26b4
Karl Thomas <karlthomas23@gmail.com>
2023-06-03 20:37:26 +0930

    Changed to arrow function expressions

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,53 @@
 // eslint-disable-next-line no-unused-vars
 import * as React from 'react';
 
-const books = [
+const list = [
   {
-  title: 'React',
-  url: 'https://reactjs.org/',
-  author: 'Jordan Walke',
-  num_comments: 3,
-  points: 4,
-  objectID: 0,
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
   },
   {
-  title: 'Redux',
-  url: 'https://redux.js.org/',
-  author: 'Dan Abramov, Andrew Clark',
-  num_comments: 2,
-  points: 5,
-  objectID: 1,
-},
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
 ];
 
-const reduxBooks = books.filter(book => {
-  return book.title === 'Redux';
-});
-
-function App() {
-
-  return (
+const App = () => 
     <div>
       <h1>My Hacker Stories</h1>
-      
-      <ul>
-        {
-          reduxBooks.map((book) => <li key={book.id}>{book.title}</li> )
 
-
-
-          //books.filter(function(book){
-          //  if (book.title === 'Redux') {
-          //    return book.title
-          //  }
-          //})
-        }
-
-        {
-          //books.map((book) => <li key={book.id}>{book.title}</li> )
-        }
-      </ul>
-
-      <label htmlFor='name'>Search: </label>
-      <input type="text" id="name" />
+      <Search />
+      <hr />
+      <List />
     </div>
-  );
-}
+
+const Search = () => 
+    <div>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" />
+    </div>
+
+const List = () => 
+    <ul>
+      { list.map((item) => 
+          <li key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+          </li>
+        )}
+    </ul>
+
+
 export default App;
